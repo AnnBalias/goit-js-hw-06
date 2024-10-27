6903d99d6e764c4d343db450c4d363d6b90df34c
AnnBalias <annbalias@gmail.com>
2024-10-27 11:55:40 +0200

    Homework 6

--- js/task-3.js
+++ js/task-3.js
@@ -1,10 +1,32 @@
-function getElementWidth(content, padding, border) {
-    content = Number.parseFloat(content);
-    padding = Number.parseFloat(padding);
-    border = Number.parseFloat(border);
-    return content + padding * 2 + border * 2;
+class StringBuilder {
+    #value = [];
+    constructor(initialValue) {
+        this.#value = initialValue.split("");
+    }
+
+    getValue() {
+        return this.#value.join("");
+    }
+    
+    padEnd(str) {
+        this.#value.push(str);
+    }
+    
+    padStart(str) {
+        this.#value.unshift(str);
+    }
+    
+    padBoth(str) {
+        this.#value.push(str);
+        this.#value.unshift(str);
+    }
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
